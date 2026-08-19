2a8141b64b9e12df149640eef0ea05119494dc90
Edgar Nova <ragnarok540@gmail.com>
2026-08-19 16:30:33 -0500

    adding numbers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # North
 Like Forth, but in Nim
+
+- https://skilldrick.github.io/easyforth/
--- north.nim
+++ north.nim
@@ -1,12 +1,27 @@
-var stack = newSeqOfCap[string](5)
+import std/[rdstdin, strutils]
+
+var stack = newSeqOfCap[string](256)
+var line: string
+var num: int
+
+while true:
+  let ok = readLineFromStdin("north> ", line)
+  if not ok: break
+  if line.len > 0:
+    try:
+        discard line.parseInt
+        stack.add(line)
+        echo stack
+    except ValueError:
+        if line == "+":
+            let a = stack.pop.parseInt
+            let b = stack.pop.parseInt
+            let c = a + b
+            stack.add($c)
+            echo stack
+
+echo "bye"
 echo stack
-stack.add("3")
-stack.add("2")
-stack.add("+")
-echo stack
-echo stack.pop
-echo stack.pop
-echo stack.pop
 
 # mkdir -p bin
 # nim c -o:bin/north north.nim
